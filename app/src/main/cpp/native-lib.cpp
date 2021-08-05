d12a97b473bd2b2312bc830348aa152100085cc4
Jesen0823 <36436771+Jesen0823@users.noreply.github.com>
2021-08-06 04:59:07 +0800

    [JNI_CMAKE] 05.验证三方库fmod集成结果

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,24 @@
 #include <jni.h>
 #include <string>
+#include <fmod.hpp>
+#include <android/log.h>
+
+using namespace FMOD;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_ndk_1jni_1cmake_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+    System *system;
+    System_Create(&system);
+    unsigned int version;
+    system->getVersion(&version);
+    __android_log_print(ANDROID_LOG_ERROR,"TEST","FMOD Version:%08x",version);
+
+    char *vs = (char *) malloc(sizeof(char) * 16);
+    sprintf(vs,"%08x",version);
     std::string hello = "Hello from C++";
+    hello = hello.append("\nFMOD Version:").append(vs);
     return env->NewStringUTF(hello.c_str());
 }
